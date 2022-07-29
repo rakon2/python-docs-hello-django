a912f1b4ac7e1a62c00dc220b6b2ff6b9a36793e
avyukti1 <82049896+avyukti1@users.noreply.github.com>
2022-07-29 18:08:35 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello  Axis Batch team, welcome To cloud Computing")
+    return HttpResponse("Hello  Axis Batch team, Good evening & welcome To cloud Computing - It's a great Journey")
